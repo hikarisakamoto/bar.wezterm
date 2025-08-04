e1cd00a00d1598f4834a0098826fedb917d8faa1
Hikari Sakamoto <hikarisakamoto@gmail.com>
2025-08-04 15:56:10 +0100

    reverting cell adder

--- plugin/bar/mode.lua
+++ plugin/bar/mode.lua
@@ -20,10 +20,19 @@ end
 
 M.get_foreground = function(mode, palette, utilities, options)
     if mode == "normal_mode" then
-        return palette.ansi[5]
+        return {
+            Foreground = {
+                Color = palette.ansi[5]
+            }
+        }
     end
 
-    return palette.ansi[2]
+    return {
+        Foreground = {
+            Color = palette.ansi[2]
+        }
+    }
+
 end
 
 M.get_background = function(mode, palette, utilities, options)
@@ -33,8 +42,9 @@ M.get_text = function(mode, palette, utilities, options)
     local text = mode:gsub("_mode", "")
     text = text:upper()
 
-    return options.modules.mode.icon .. utilities._space(text, options.separator.space)
-
+    return {
+        Text = options.modules.mode.icon .. utilities._space(text, options.separator.space)
+    }
 end
 
 return M
