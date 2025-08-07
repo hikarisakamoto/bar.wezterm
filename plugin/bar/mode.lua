dcf738dc3a3d98ec9b4f865f8d1addac8c2cdd64
Hikari Sakamoto <hikarisakamoto@gmail.com>
2025-08-07 21:44:41 +0100

    no fix for border

--- plugin/bar/mode.lua
+++ plugin/bar/mode.lua
@@ -24,7 +24,8 @@ end
 
 M.get_background_color = function(mode, palette)
     if mode == "normal_mode" then
-        return palette.tab_bar.background or palette.background or "transparent"
+        -- return palette.tab_bar.background or palette.background or "transparent"
+        return palette.ansi[6]
     elseif mode == "copy_mode" then
         return palette.ansi[11]
     elseif mode == "select_mode" then
@@ -36,9 +37,9 @@ end
 M.get_text = function(mode, utilities, options)
     local text = mode:gsub("_mode", "")
     text = text:upper()
-
+    return options.modules.mode.icon .. utilities._space(text, options.separator.space)
     -- return options.modules.mode.icon .. utilities._space(text, options.separator.space, 0)
-    return options.modules.mode.icon .. " " .. text
+    -- return options.modules.mode.icon .. " " .. text
 end
 
 return M
