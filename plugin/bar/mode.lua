3d67c47ed4f6bbf772573cdf716a23f1203d7167
Hikari Sakamoto <hikarisakamoto@gmail.com>
2025-08-17 17:57:00 +0100

    Adding dynamic icon

--- plugin/bar/mode.lua
+++ plugin/bar/mode.lua
@@ -1,3 +1,4 @@
+local wez = require "wezterm"
 local M = {}
 
 ---Return active key table mode as a user friendly name
@@ -24,12 +25,7 @@ end
 
 M.get_background_color = function(mode, palette)
     if mode == "normal_mode" then
-        -- return palette.tab_bar.background or palette.background or "transparent"
         return palette.background or "transparent"
-    elseif mode == "copy_mode" then
-        return palette.ansi[3]
-    elseif mode == "select_mode" then
-        return palette.ansi[4]
     end
     return palette.brights[8]
 end
@@ -37,8 +33,15 @@ end
 M.get_text = function(mode, utilities, options)
     local text = mode:gsub("_mode", "")
     text = text:upper()
-    -- return options.modules.mode.icon .. utilities._space(text, options.separator.space)
-    -- return options.modules.mode.icon .. utilities._space(text, options.separator.space, 0)
+    if mode == "normal_mode" then
+        return wez.nerdfonts.cod_screen_normal .. " " .. text
+    end
+    if mode == "copy_mode" then
+        return wez.nerdfonts.cod_copy .. " " .. text
+    end
+    if mode == "search_mode" then
+        return wez.nerdfonts.cod_search .. " " .. text
+    end
     return options.modules.mode.icon .. " " .. text
 end
 
